a81a6dd4750dd7f7873d05a3ed1a8bb322b3ecc8
DevuKAnil <93868381+DevuKAnil@users.noreply.github.com>
2022-01-29 21:12:23 +0530

    Add files via upload

---
+++ CO2/CO2_11.py
@@ -0,0 +1,7 @@
+import math
+t_area=lambda b,h:1/2*b*h
+r_area=lambda l,b:l*b
+s_area=lambda a:a*a
+print("area of triangle:",t_area(10,20))
+print("area of rectangle:",r_area(30,20))
+print("area of square:",s_area(15))
